e0f3f11557254d929de346810e965fc6c9fe5224
vivek <saiviveknakirikanti@gmail.com>
2025-06-13 15:08:12 +0530

    intial

--- src/app/studashboard/studashboard.component.ts
+++ src/app/studashboard/studashboard.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-studashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './studashboard.component.html',
-  styleUrl: './studashboard.component.css'
+  styleUrls: ['./studashboard.component.css']
 })
 export class StudashboardComponent {
+  students: { name: string; grade: string }[] = [];
 
+  studentName = '';
+  studentGrade = '';
+
+  addStudent(nameInput: HTMLInputElement, gradeInput: HTMLInputElement) {
+    if (this.studentName&& this.studentGrade) {
+      this.students.push({ name: this.studentName, grade: this.studentGrade.toUpperCase()});
+      this.studentName = '';
+      this.studentGrade = '';
+      nameInput.value = '';
+      gradeInput.value = '';
+    }
+  }
+
+  
 }
